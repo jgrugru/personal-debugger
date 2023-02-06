df1e43dd828eb76e6b96b8985f8c27e52fe23009
jgrugru <jeff.gruenbaum@gmail.com>
2023-02-06 11:01:43 -0600

    updated timeit function

--- src/timeit_tools.py
+++ src/timeit_tools.py
@@ -1,10 +1,11 @@
 from typing import Callable, List
 import time
 import statistics
-from loguru import logger
 
 
-def time_my_function(fn: Callable, n: int = 100, precision: int = 6, *args, **kwargs) -> List[float]:
+def time_my_function(
+    fn: Callable, n: int = 100, precision: int = 6, output: bool = True, *args, **kwargs
+) -> List[float]:
     """
     Timing function\n
     Provides mean, median, stdev, min, and max time for fn call. Runs the given function n times, default 100.
@@ -22,5 +23,7 @@ def time_my_function(fn: Callable, n: int = 100, precision: int = 6, *args, **kw
     minimum = round(min(list_of_times), precision)
     maximum = round(max(list_of_times), precision)
 
-    logger.info(f"{n} times: mean={average}, median={round(statistics.median(list_of_times), precision)}, stdev={std_dev}, min={minimum}, max={maximum}")
+    if output:
+        print(f"{n} times: mean={average}, median={round(statistics.median(list_of_times), precision)}, stdev={std_dev}, min={minimum}, max={maximum}")
+
     return list_of_times
